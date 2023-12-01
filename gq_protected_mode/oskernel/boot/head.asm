b37683d68eddf9f330c7cbb1e480a3c1ae5e4d43
stark <1753800647@qq.com>
2023-12-01 00:51:36 -0800

    增加了C语言于汇编之间互调，清除编译文件

--- gq_protected_mode/oskernel/boot/head.asm
+++ gq_protected_mode/oskernel/boot/head.asm
@@ -1,6 +1,10 @@
 [SECTION .text]
 [BITS 32]
 extern kernel_main
+extern c_test
+
+global var
+var db 'Q'
 
 global _start
 
@@ -8,3 +12,10 @@ _start:
     call kernel_main
 
     jmp $
+
+;void gq_ctest(void)
+global gq_ctest
+gq_ctest:
+    call c_test
+
+    ret
--- gq_protected_mode/oskernel/init/main.c
+++ gq_protected_mode/oskernel/init/main.c
@@ -1,10 +1,22 @@
 //
 // Created by ziya on 22-6-23.
 //
+extern void gq_ctest(void);
+extern char var;
 
 void kernel_main(void) {
     int a = 0;
 
     char* video = (char*)0xb8000;
     *video = 'G';
+
+    gq_ctest();
+    *video = var;
+}
+
+void c_test(void) {
+    int a = 0;
+
+    char* video = (char*)0xb8000;
+    *video = 'Z';
 }
